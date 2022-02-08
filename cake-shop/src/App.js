722f3a6526370b876f2e3bd6f0a740587e7adbb6
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-02-08 17:19:36 +0530

    React Redux Setup

--- cake-shop/src/App.js
+++ cake-shop/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
     <div className="App">
-      Let us Learn React Redux
+      <CakeContainer />
     </div>
   );
 }
